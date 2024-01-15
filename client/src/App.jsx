a948999bad4ea48c3a7b5888d82c4fc4946a0938
oguzsoft <oguzkaangunduz@gmail.com>
2024-01-15 17:45:29 +0300

    Add show page on client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import PostsList from '../features/posts/PostsList.jsx'; 
-function App() {
-  // const [count, setCount] = useState(0)
+import NavBar from '../components/NavBar.jsx';
+import AppRoutes from '../components/AppRoutes.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+function App() {
   return (
-    <>
-    <div className="app">
-      <h1>React on Rails App</h1>
-      <p>You can find  this file in client/src/App.jsx</p>
-      <PostsList />
-    </div>
-    </>
+    <Router>
+      <div className="app">
+        <h1>React on Rails App</h1>
+        <p>You can find  this file in client/src/App.jsx</p>
+        <NavBar></NavBar>
+        <AppRoutes />
+      </div>
+    </Router>
   )
 }
 
